dccbfc0c0b388ca055d9f5c5b12574376219db24
Krecik <Marcin.Jarzebski94@gmail.com>
2018-01-12 00:14:01 +0100

    renames and printouts

--- SR/ticket_server.py
+++ SR/ticket_server.py
@@ -6,16 +6,26 @@ After launching the server call localhost:8000 in browser to test if it's workin
 """
 
 
-class MyHandler(SimpleHTTPRequestHandler):
-    # GET method override
-    def do_GET(self):
+class TicketServerRequestHandler(SimpleHTTPRequestHandler):
+
+    def do_method(self, caller_method_name):
+        print(caller_method_name)
+
+
         self.send_response(200)
         self.end_headers()
         self.wfile.write(self.requestline.encode())
 
+    def do_POST(self):
+        self.do_method("POST")
+
+    # GET method override
+    def do_GET(self):
+        self.do_method("GET")
+
 
 # Creating server instance
-httpd = HTTPServer(("", 8000), MyHandler)
+httpd = HTTPServer(("", 8000), TicketServerRequestHandler)
 
 # Launching the server (no option to stop, to stop call 'shutdown' from another thread)
 httpd.serve_forever()

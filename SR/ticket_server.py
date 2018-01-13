75acc99bdbcacc1862ba3d310b6ff2497b4080f6
Krecik <Marcin.Jarzebski94@gmail.com>
2018-01-13 06:02:24 +0100

    ticket server rework

--- SR/ticket_server.py
+++ SR/ticket_server.py
@@ -1,20 +1,20 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import threading
 import time
+
 """"
 Test handler that returns requestline
 After launching the server call localhost:8000 in browser to test if it's working
 """
 
-class AirportServer():
 
-    def __init__(self):
-        # Table of available tickets linked to transactions
-        self.IMPLEMENT_THIS = "test"
-        # TODO: think about data structure
+class AirportSupervisor:
+    def __init__(self, port):
+        # server instance
+        # "" means to listen on all available interfaces
+        self.http_server = HTTPServer(("", port), TicketServerRequestHandler)
 
-        #TODO: initialize handler thread for easier shutdown
-        self.http_server = HTTPServer(("", 8000), TicketServerRequestHandler)
+        # server thread, for easier cleanup
         self.server_thread = threading.Thread(target=self.http_server.serve_forever)
 
     def start_server(self):
@@ -24,16 +24,17 @@ class AirportServer():
         self.http_server.shutdown()
         self.server_thread.join()
 
+
 class TicketServerRequestHandler(SimpleHTTPRequestHandler):
 
     def do_method(self, caller_method_name):
         print(caller_method_name)
 
-
         self.send_response(200)
         self.end_headers()
         self.wfile.write(self.requestline.encode())
 
+    # POST method override
     def do_POST(self):
         self.do_method("POST")
 
@@ -42,13 +43,13 @@ class TicketServerRequestHandler(SimpleHTTPRequestHandler):
         self.do_method("GET")
 
 
-def main():
-
-    test=AirportServer()
+def main(port):
+    test = AirportSupervisor(port)
 
     test.start_server()
     time.sleep(4)
     test.stop_server()
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    main(8000)

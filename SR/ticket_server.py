d9aa2c367f9a5861e84f205b5cb0be52be81e43f
Krecik <Marcin.Jarzebski94@gmail.com>
2018-01-13 17:48:34 +0100

    data tables implementations, passing arguments into request handler experiments

--- SR/ticket_server.py
+++ SR/ticket_server.py
@@ -7,6 +7,10 @@ Test handler that returns requestline
 After launching the server call localhost:8000 in browser to test if it's working
 """
 
+# Globals for easier parameter injection into request handler
+airport_ID = None
+initial_ticket_quantities = None
+
 
 class AirportSupervisor:
     def __init__(self, port):
@@ -25,14 +29,41 @@ class AirportSupervisor:
         self.server_thread.join()
 
 
+class TicketDataManager:
+    # Hardcoded information about which airport listens on what port
+    airport_ID_to_port_map = {
+        'AirportA': 8000,
+        'AirportB': 8001,
+        'AirportC': 8002,
+        'AirportD': 8003
+    }
+
+    # Hardcoded information about which airport handles which tickets
+    ticket_ID_to_airpot_ID_map = {
+        'Ticket0': 'AirportA',
+        'Ticket1': 'AirportA',
+        'Ticket2': 'AirportB',
+        'Ticket3': 'AirportB',
+        'Ticket4': 'AirportB',
+        'Ticket5': 'AirportC',
+        'Ticket6': 'AirportD',
+        'Ticket7': 'AirportD',
+    }
+
+    def __init__(self):
+        self.ID = airport_ID
+
+
 class TicketServerRequestHandler(SimpleHTTPRequestHandler):
+
+    ticket_data_manager = TicketDataManager()
 
     def do_method(self, caller_method_name):
         print(caller_method_name)
 
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(self.requestline.encode())
+        self.wfile.write((TicketServerRequestHandler.ticket_data_manager.ID).encode())
 
     # POST method override
     def do_POST(self):
@@ -43,7 +74,10 @@ class TicketServerRequestHandler(SimpleHTTPRequestHandler):
         self.do_method("GET")
 
 
-def main(port):
+def main(port, ID, ticket_quantities):
+
+    global airport_ID
+    airport_ID = ID
     test = AirportSupervisor(port)
 
     test.start_server()
@@ -52,4 +86,4 @@ def main(port):
 
 
 if __name__ == "__main__":
-    main(8000)
+    main(8000, "test_param", None)

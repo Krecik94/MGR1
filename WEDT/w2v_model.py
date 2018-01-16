f5c356208dd54c3108d22bc9c6c54f4ad0e96495
wolframowy <wolframowy@o2.pl>
2018-01-16 22:16:06 +0100

    Change w2v_model creator. Load list of paths. Return w2v model

--- WEDT/w2v_model.py
+++ WEDT/w2v_model.py
@@ -2,27 +2,24 @@ from gensim.models import word2vec
 import re
 
 
-def create_model(path1, path2):
+def create_m2v_model(pathList):
 
     newLineRegex = re.compile('\n')
 
     sentences = []
 
-    for line in open(path1, 'r'):
-        sentence = line.split(' ')
-        sentence[len(sentence)-1] = newLineRegex.sub('', sentence[len(sentence)-1])
-        sentences.append(sentence)
+    if len(pathList) > 0:
+        for path in pathList:
+            for line in open(path, 'r'):
+                sentence = line.split(' ')
+                sentence[len(sentence)-1] = newLineRegex.sub('', sentence[len(sentence)-1])
+                sentences.append(sentence)
 
-    for line in open(path2, 'r'):
-        sentence = line.split(' ')
-        sentence[len(sentence)-1] = newLineRegex.sub('', sentence[len(sentence)-1])
-        sentences.append(sentence)
+        w2v = word2vec.Word2Vec(sentences)
 
-    w2v = word2vec.Word2Vec(sentences)
-
-    return w2v
+        return w2v
 
 
 if __name__ == '__main__':
-    create_model('Data/ISTS/test_data_w_fold_standard/STSint.input.headlines.sent1.txt',
-                 'Data/ISTS/test_data_w_fold_standard/STSint.input.headlines.sent2.txt')
+    create_m2v_model(['Data/ISTS/test_data_w_fold_standard/STSint.input.headlines.sent1.txt',
+                     'Data/ISTS/test_data_w_fold_standard/STSint.input.headlines.sent2.txt'])

342a5126203f7946d30392e71e847efe4c193167
wolframowy <wolframowy@o2.pl>
2018-01-16 16:07:32 +0100

    Add simpler cost function. Fix loaded words iterations

--- WEDT/neural_test_SemVal.py
+++ WEDT/neural_test_SemVal.py
@@ -6,40 +6,50 @@ import numpy as np
 input_data1 = []
 input_data2 = []
 gs = []
-input_file = open("Data/STS/Train_data/STS.input.images.txt", "r")
-gs_file = open("Data/STS/Train_data/STS.gs.images.txt", "r")
+input_file = open("Data/STS/Train_data/STS.input.headlines.txt", "r")
+gs_file = open("Data/STS/Train_data/STS.gs.headlines.txt", "r")
 
-#sentences = word2vec.Text8Corpus('text8')
-w2v = word2vec.KeyedVectors.load_word2vec_format('deps.words')#word2vec.Word2Vec(sentences, size=200)#
+# sentences = word2vec.Text8Corpus('text8')
+w2v = word2vec.KeyedVectors.load_word2vec_format('deps.words')  # word2vec.Word2Vec(sentences, size=200)#
 
-#w2v.wv.save_word2vec_format('test')
+# w2v.wv.save_word2vec_format('test')
 
-#Regex for taking out special characters
-regex = re.compile('[^a-zA-Z]')
+# Regex for taking out special characters
+wordRegex = re.compile('[^a-zA-Z]')
+
+numRegex = re.compile('\n')
 
 for line in input_file:
     if line != "\n":
         val = line.split("\t")
-        tmp1 = []
-        tmp2 = []
+        tmp1 = np.array([])
+        tmp2 = np.array([])
         split = re.split('\s|-', val[1])
-        for x in re.split('\s|-', val[0]):  #.split(" "):
-            if regex.sub('', x.lower()) != '':
-                tmp1.append(w2v.wv[regex.sub('', x.lower())])
-        for x in re.split('\s|-', val[1]):  #.split(" "):
-            if regex.sub('', x.lower()) != '':
-                tmp2.append(w2v.wv[regex.sub('', x.lower())])
+        for x in re.split('\s|-', val[0]):  # .split(" "):
+            if wordRegex.sub('', x.lower()) != '':
+                tmp1 = np.append(tmp1, w2v.wv[wordRegex.sub('', x.lower())])
+                # if tmp1.size:
+                #     tmp1 = np.concatenate((tmp1, w2v.wv[wordRegex.sub('', x.lower())]))
+                # else:
+                #     tmp1 = w2v.wv[wordRegex.sub('', x.lower())]
+        for x in re.split('\s|-', val[1]):  # .split(" "):
+            if wordRegex.sub('', x.lower()) != '':
+                tmp2 = np.append(tmp2, w2v.wv[wordRegex.sub('', x.lower())])
+                # if tmp1.size:
+                #     tmp2 = np.concatenate((tmp2, w2v.wv[wordRegex.sub('', x.lower())]))
+                # else:
+                #     tmp2 = w2v.wv[wordRegex.sub('', x.lower())]
         input_data1.append(tmp1)
         input_data2.append(tmp2)
 for line in gs_file:
     if line != "\n":
-        gs.append(line)
+        gs.append(float(numRegex.sub('', line)))
 
 input_data = []
 for x in range(len(input_data1)):
-    input_data1[x].extend([0] * (30 - len(input_data1[x])))
-    input_data2[x].extend([0] * (30 - len(input_data2[x])))
-    input_data.append((input_data1[x], input_data2[x]))
+    # input_data1[x].extend([0] * (30 - len(input_data1[x])))
+    # input_data2[x].extend([0] * (30 - len(input_data2[x])))
+    input_data.append((input_data1[x], input_data2[x], gs[x]))
 
 
 n_nodes_hl1 = 500
@@ -49,9 +59,9 @@ n_classes = 1
 batch_size = 100
 
 
-x = tf.placeholder(tf.float32, [None, 30])
-x2 = tf.placeholder(tf.float32, [None, 30])
-y = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32, [None, None])  # first sentence
+x2 = tf.placeholder(tf.float32, [None, None])  # second sentence
+y = tf.placeholder(tf.float32)  # target value
 
 def neural_network_model(data, data2):
     """
@@ -64,10 +74,10 @@ def neural_network_model(data, data2):
     Definition of weights and biases in each node in layer.
     At start they are defined as random
     """
-    hidden_1_layer_1 = {'weights': tf.Variable(tf.random_normal([30, n_nodes_hl1])),
+    hidden_1_layer_1 = {'weights': tf.Variable(tf.random_normal([9000, n_nodes_hl1])),
                         'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
-    hidden_1_layer_2 = {'weights': tf.Variable(tf.random_normal([30, n_nodes_hl1])),
+    hidden_1_layer_2 = {'weights': tf.Variable(tf.random_normal([9000, n_nodes_hl1])),
                         'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {'weights1': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
@@ -80,7 +90,6 @@ def neural_network_model(data, data2):
     """
     Definition of nodes in layers
     """
-    print(data[2:-2])
     l1_1 = tf.add(tf.matmul(data, hidden_1_layer_1['weights']), hidden_1_layer_1['biases'])
     l1_1 = tf.nn.relu(l1_1)
 
@@ -90,7 +99,7 @@ def neural_network_model(data, data2):
     l2 = tf.add(tf.add(tf.matmul(l1_1, hidden_2_layer['weights1']), tf.matmul(l1_2, hidden_2_layer['weights2'])), hidden_2_layer['biases'])
     l2 = tf.nn.relu(l2)
 
-    output = tf.matrix_transpose(tf.matmul(l2, output_layer['weights']) + output_layer['biases'])
+    output = tf.matmul(l2, output_layer['weights']) + output_layer['biases']
 
     return output
 
@@ -101,20 +110,29 @@ def train_neural_network(x, x2):
     :return: none
     """
     prediction = neural_network_model(x, x2)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    cost = tf.reduce_mean(tf.abs(tf.subtract(prediction, y)))  # tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y)) TODO
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(cost)
 
     hm_epochs = 20
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            _, c = sess.run([optimizer, cost], feed_dict={x: input_data1, x2: input_data2, y: gs})
-            epoch_loss += c
+            for fst, scnd, thrd in input_data:
+                fst_res = np.pad(fst, (0, 9000 - fst.shape[0]), 'constant', constant_values=0.0)
+                scnd_res = np.pad(scnd, (0, 9000 - scnd.shape[0]), 'constant', constant_values=0.0)
+                fst_res.shape = (1, fst_res.shape[0])
+                scnd_res.shape = (1, scnd_res.shape[0])
+                _, c = sess.run([optimizer, cost], feed_dict={x: fst_res, x2: scnd_res, y: thrd})
+                epoch_loss += c
 
             print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
-        correct = tf.equal(tf.argmax(prediction), tf.argmax(y))
+        correct = tf.equal(prediction, y)
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: input_data1,x2: input_data2, y: gs}))
+        fst_test = np.pad(input_data[0][0], (0, 9000 - input_data[0][0].shape[0]), 'constant', constant_values=0.0)
+        scnd_test = np.pad(input_data[0][1],(0, 9000 - input_data[0][1].shape[0]), 'constant', constant_values=0.0)
+        fst_test.shape = (1, fst_test.shape[0])
+        scnd_test.shape = (1, scnd_test.shape[0])
+        print('Accuracy:', accuracy.eval({x: fst_test, x2: scnd_test, y: input_data[0][2]}))
 
 train_neural_network(x, x2)

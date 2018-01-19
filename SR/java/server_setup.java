765a1d4717b8ce347a6c03923031755f454736ee
wolframowy <wolframowy@o2.pl>
2018-01-19 22:28:55 +0100

    Finish server_setup, Add basic GET and POST to ticket_server

--- SR/java/server_setup.java
+++ SR/java/server_setup.java
@@ -1,13 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
 
 class server_setup {
+
+    static Map<String, Integer> airport_a_ticket_quantity_dict = new HashMap<>();
+    static Map<String, Integer> airport_b_ticket_quantity_dict = new HashMap<>();
+    static Map<String, Integer> airport_c_ticket_quantity_dict = new HashMap<>();
+    static Map<String, Integer> airport_d_ticket_quantity_dict = new HashMap<>();
+
+    static Map<String, Map<String, Integer>> airport_data = new HashMap<>();
+
+    private static void fillMaps() {
+        airport_a_ticket_quantity_dict.put("Ticket0", 8);
+        airport_a_ticket_quantity_dict.put("Ticket1", 8);
+
+        airport_b_ticket_quantity_dict.put("Ticket2", 1);
+        airport_b_ticket_quantity_dict.put("Ticket3", 1);
+        airport_b_ticket_quantity_dict.put("Ticket4", 1);
+
+        airport_c_ticket_quantity_dict.put("Ticket5", 2);
+
+        airport_d_ticket_quantity_dict.put("Ticket6", 1);
+        airport_d_ticket_quantity_dict.put("Ticket7", 1);
+
+        airport_data.put("AirportA", airport_a_ticket_quantity_dict);
+        airport_data.put("AirportB", airport_b_ticket_quantity_dict);
+        airport_data.put("AirportC", airport_c_ticket_quantity_dict);
+        airport_data.put("AirportD", airport_d_ticket_quantity_dict);
+
+    }
+
     public static void main(String args[]) {
         try {
-            ticket_server.run(8000);
-            ticket_server.run(8001);
-            ticket_server.run(8002);
+            fillMaps();
+
+            Integer startPort = 8000;
+            int i = 0;
+            for (Map.Entry<String, Map<String, Integer>> entry : airport_data.entrySet())
+            {
+                ticket_server.run(entry.getKey(), entry.getValue(), startPort + i);
+                ++i;
+            }
         }
         catch (Exception e) {
-            System.out.print(e);
+            System.out.println(e);
         }
     }
 }
--- SR/java/ticket_server.java
+++ SR/java/ticket_server.java
@@ -10,31 +10,44 @@ import com.sun.net.httpserver.HttpServer;
 public class ticket_server {
 
     private String airport_ID;
+    private TicketDataManager dataManager;
     //private Map<String, Integer>
 
-    public static void run(int port) throws Exception {
+    public void run(String airport_ID, Map<String, Integer> airport_data, int port) throws Exception {
+        this.airport_ID = airport_ID;
+        this.dataManager = new TicketDataManager(airport_ID, airport_data);
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/test", createHandler(this.dataManager));
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            // gets what method was used (GET, POST, etc.)
-            String response;
-            if(t.getRequestMethod() == "GET")
-                response = "This is the response GET";
-            else if (t.getRequestMethod() == "POST")
-                response = "This is the response " + t.getRequestMethod();
-            else
-                response = "This is the GENERIC response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+    static HttpHandler createHandler (TicketDataManager dataManager) {
+
+        class MyHandler implements HttpHandler {
+            @Override
+            public void handle(HttpExchange t) throws IOException {
+                // gets what method was used (GET, POST, etc.)
+                String response;
+                switch (t.getRequestMethod()) {
+                    case "POST" :
+                        response = "This is the response " + t.getRequestMethod();
+                        break;
+                    case "GET" :
+                        response = "This is the response GET";
+                        break;
+                    default:
+                        response = "This is the GENERIC response";
+                        break;
+                }
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
         }
+
+        return new MyHandler();
     }
 
 }

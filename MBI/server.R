c5456df678d230413afeb2b390a3ebff93a6fc45
Krecik <Marcin.Jarzebski94@gmail.com>
2018-01-30 20:52:14 +0100

    removed old logic

--- MBI/server.R
+++ MBI/server.R
@@ -12,40 +12,14 @@ function(input, output) {
   #print(input_BAM_for_debug_2)
   
   
-  
-  # Reading input files
-  input_file_1 <- BamFile("wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam")
-  input_file_2 <- BamFile("wgEncodeUwRepliSeqBg02esG2AlnRep1.bam")
-  
-  BAM1 = readBAM("wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam")
-  BAM2 = readBAM("wgEncodeUwRepliSeqBg02esG2AlnRep1.bam")
+  BAM1 = readBAM("wgEncodeUwRepliSeqMcf7S1AlnRep1.bam")
+  BAM2 = readBAM("wgEncodeUwRepliSeqMcf7S2AlnRep1.bam")
   
 
   # Defining action on button press
-  observeEvent(input$button, {
-    # Creating ranges from which to extract sequences
-    input_rages <- GRanges(input$name,IRanges(input$rangeStart, input$rangeEnd))
-    
-    # Reading sequences
-    reads_1 <- scanBam(input_file_1, param=ScanBamParam(what=c("rname", "seq", "pos"), which=input_rages))
-    reads_2 <- scanBam(input_file_2, param=ScanBamParam(what=c("rname", "seq", "pos"), which=input_rages))
-    
-    names(reads_1) <- "val"
-    names(reads_2) <- "val"
-    
-    # Extracting first sequence hit
-    first_string_1 <-reads_1$val$seq[1]
-    first_string_2 <-reads_2$val$seq[1]
-    
-    # Debug printouts
-    print(toString(first_string_1))
-    print(toString(first_string_2))
-    
-    # Assigning values to output fields
-    output$sequence1 <- renderText(toString(first_string_1))
-    output$sequence2 <- renderText(toString(first_string_2))
-  })
   
+  
+  # Drawing the coverage plot
   output$coverage_plot <- renderPlot({
     chr1 <- BAM1[BAM1$rname == input$name,'pos']
     chr2 <- BAM2[BAM2$rname == input$name,'pos']
--- MBI/ui.R
+++ MBI/ui.R
@@ -14,28 +14,13 @@ fluidPage(
       # Name
       textInput(inputId = "name",
                    label = "Name:",
-                   value = "chr1"),
-      # Range start
-      numericInput(inputId = "rangeStart",
-                  label = "Range start:",
-                  value = 10000),
-      # Range end
-      numericInput(inputId = "rangeEnd",
-                   label = "Range end:",
-                   value = 15000),
-      # Button
-      actionButton(inputId = "button",
-                   label = "Calculate")
+                   value = "chr1")
+
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      # Sequence from 1st BAM file
-      textOutput(outputId = "sequence1"),
-      # Sequence from 2nd BAM file
-      textOutput(outputId = "sequence2"),
-      
       plotOutput("coverage_plot")
     )
   )

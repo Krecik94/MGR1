192b2b4a5b8ab08760e8929e98563d3bab16ac6f
Krecik <Marcin.Jarzebski94@gmail.com>
2018-12-20 23:29:40 +0100

    added script to create output file

--- SNR/inputParser.py
+++ SNR/inputParser.py
@@ -52,17 +52,42 @@ def make_csvs_one_line():
 
 
 def make_one_big_file():
-    with open(os.path.join('obrazki', 'Test_all.csv'), 'w') as big_file:
-        for dirpath, dirnames, filenames in os.walk('obrazki\\fruits-360\\Test'):
-                print(dirpath)
-                for f in filenames:
-                    if f.endswith('.csv'):
-                        with open(os.path.join(dirpath, f), 'r') as file:
-                            content = file.read()
-                            content_one_line = content.replace('\n', ',')
-                            content_one_line = content_one_line[:-1]
-                            big_file.write(content_one_line)
-                            big_file.write('\n')
+    with open(os.path.join('obrazki', 'Training_all.csv'), 'w') as big_file:
+        for dirpath, dirnames, filenames in os.walk('obrazki\\fruits-360\\Training'):
+            print(dirpath)
+            for f in filenames:
+                if f.endswith('.csv'):
+                    with open(os.path.join(dirpath, f), 'r') as file:
+                        content = file.read()
+                        content_one_line = content.replace('\n', ',')
+                        content_one_line = content_one_line[:-1]
+                        big_file.write(content_one_line)
+                        big_file.write('\n')
+
+
+def make_one_big_output_file():
+    dirname_set = set()
+    for dirpath, dirnames, filenames in os.walk('obrazki\\fruits-360\\Training'):
+        for dirname in dirnames:
+            dirname_set.add(dirname)
+    dirname_list = list(dirname_set)
+    dirname_list = sorted(dirname_list)
+
+    with open(os.path.join('obrazki', 'Test_output_all.csv'), 'w') as big_file:
+        for dirpath, dirnames, filenames in os.walk('obrazki\\fruits-360\\Training'):
+            print(dirpath)
+            for f in filenames:
+                string_to_write = ''
+                if f.endswith('.csv'):
+                    for i in range(len(dirname_list)):
+                        if i == dirname_list.index(os.path.basename(dirpath)):
+                            string_to_write+='1'
+                        else:
+                            string_to_write+='0'
+                        string_to_write+=','
+                    big_file.write(string_to_write[:-1])
+                    big_file.write('\n')
+
 
 if __name__ == '__main__':
-    make_one_big_file()
+    make_one_big_output_file()

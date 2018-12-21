a9ac74f41f0afa93e04baf95d678306a790a392f
Krecik <Marcin.Jarzebski94@gmail.com>
2018-12-21 02:55:06 +0100

    added top_k accuracies and skipping rows

--- SNR/network.py
+++ SNR/network.py
@@ -1,19 +1,34 @@
+import random
+
+import keras
 from keras.layers import Dense
 import pandas as pd
 from keras.models import Sequential
 from sklearn.metrics import confusion_matrix, f1_score
+import functools
 
+top3_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
+top3_acc.__name__ = 'top3_acc'
+
+top2_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=2)
+top2_acc.__name__ = 'top2_acc'
+
+top1_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=1)
+top1_acc.__name__ = 'top1_acc'
 
 def main():
+    n = 45171  # number of records in file
+    s = 5000  # desired sample size
+    skip = sorted(random.sample(range(n), n - s))
     # Data used for training
-    test_input_data = pd.read_csv('obrazki\\Test_all.csv', header=None)
+    test_input_data = pd.read_csv('obrazki\\Test_all.csv', header=None, skiprows=skip)
     print('loaded test_input_data')
-    test_output_data = pd.read_csv('obrazki\\Test_output_all.csv', header=None)
+    test_output_data = pd.read_csv('obrazki\\Test_output_all.csv', header=None, skiprows=skip)
     print('loaded test_output_data')
 
-    training_input_data = pd.read_csv('obrazki\\Training_all.csv', header=None)
+    training_input_data = pd.read_csv('obrazki\\Training_all.csv', header=None, skiprows=skip)
     print('loaded training_input_data')
-    training_output_data = pd.read_csv('obrazki\\Training_output_all.csv', header=None)
+    training_output_data = pd.read_csv('obrazki\\Training_output_all.csv', header=None, skiprows=skip)
     print('loaded training_output_data')
 
     print(test_input_data)
@@ -22,25 +37,25 @@ def main():
 
     # creating model
     network_model = Sequential()
-    network_model.add(Dense(100, activation='relu', input_shape=(10000,)))
+    network_model.add(Dense(1000, activation='relu', input_shape=(10000,)))
 
     # Add one hidden layer
-    #network_model.add(Dense(500, activation='relu'))
+    #network_model.add(Dense(1000, activation='relu'))
     # Add one hidden layer
-    network_model.add(Dense(100, activation='relu'))
 
+    network_model.add(Dense(200, activation='relu'))
     # Add an output layer
     network_model.add(Dense(87, activation='softmax'))
 
     # Creating neural network
     network_model.compile(loss='categorical_crossentropy',
-                          optimizer='rmsprop',
-                          metrics=['accuracy'])
+                          optimizer='adadelta',
+                          metrics=['accuracy', 'top_k_categorical_accuracy', top1_acc, top2_acc, top3_acc])
 
     print('starting training')
     # Training neural network
     network_model.fit(training_input_data, training_output_data,
-                      batch_size=64, epochs=20)
+                      batch_size=1000, epochs=20)
     print('finished training')
 
     y_pred = network_model.predict(test_input_data)
